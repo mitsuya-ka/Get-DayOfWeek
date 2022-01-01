958810c5bd3e0dc15038f44254219261b3054e91
mitsuya-ka <69183133+mitsuya-ka@users.noreply.github.com>
2022-01-01 16:25:46 +0900

    Update Get-DayOfWeek.psm1

--- Get-DayOfWeek.psm1
+++ Get-DayOfWeek.psm1
@@ -1,13 +1,17 @@
 function Get-DayOfWeek {
 	param (
-		[Datetime] $Date
+		[Datetime] $Date,
+		[String] $PathHD = 'Holidays.txt',
+		[Switch] $WorkDay
 	)
-	# Sunday 0, Monday 1, Tuesday 2, Wednesday 3, Thursday 4, Friday 5, Saturday 6, and Holiday 7
-	# Japanease Holidays in 2021(you need update this data depend on year/country.)
-	$ar = @("2021/1/1", "2021/1/11", "2021/2/11", "2021/2/23", "2021/3/20", "2021/4/29", `
-			"2021/5/3", "2021/5/4", "2021/5/5", "2021/7/22", "2021/7/23", "2021/8/8", `
-			"2021/8/9", "2021/9/20", "2021/9/23", "2021/11/3", "2021/11/23")
-	$Holidays = $ar | ForEach-Object { [Datetime]$_ }
+	# Sunday 0, Monday 1, Tuesday 2, Wednesday 3, Thursday 4, Friday 5, Saturday 6, Holiday 7
+	# Holidays data from "https://uic.jp/calendar/"
+	if ( $WorkDay ) {
+		[Datetime[]]$Holidays = (Get-Content -Path $PathHD | Where-Object { $_ -notmatch '^#' }).Replace('A ', '')
+	}
+	else {
+		[Datetime[]]$Holidays = Get-Content -Path $PathHD | Where-Object { $_ -notmatch '^[#A]' }
+	}
 	if ( $Date.Date -in $Holidays ) {
 		enum MyDayOfWeek { Holiday = 7 }
 		[MyDayOfWeek]::Holiday
